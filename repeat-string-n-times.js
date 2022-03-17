7fb9aa913a8692dc031c1db713e2aacd51d33894
doubledizle <donnyduncan@gmail.com>
2022-03-16 21:23:21 -0500

    Create repeat-string-n-times.js

---
+++ repeat-string-n-times.js
@@ -0,0 +1,15 @@
+// Write a function that takes a number and a string, and repeates the string n times
+
+function repeatStr (n, s) {
+  let total = ''
+  for (i = 1; i <= n; i++){
+    total += s;
+  }
+  return total;
+}
+
+// or simplified
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
