b47b266cea5a85de6d1c951e3d251e8c7f334340
doubledizle <donnyduncan@gmail.com>
2022-03-19 14:52:28 -0500

    Create sum-from-1-to-num.js

---
+++ sum-from-1-to-num.js
@@ -0,0 +1,9 @@
+// Write a function that takes a number and returns the sum of all numbers from 1 to that number
+
+var summation = function (num){
+  let total = 0
+  for (i = 1 ; i <= num; i++){
+    total += i
+  }
+  return total
+}
