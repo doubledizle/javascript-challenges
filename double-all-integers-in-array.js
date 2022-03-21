57a85350bb64863dda56a0a93f04947d76c1c44e
doubledizle <donnyduncan@gmail.com>
2022-03-21 00:06:51 -0500

    Create double-all-integers-in-array.js

---
+++ double-all-integers-in-array.js
@@ -0,0 +1,17 @@
+// Write a function that takes an array and creates a new array with all integers doubled
+
+function maps(x){
+  let newArr = []
+  x.forEach (n => newArr.push(n * 2))
+  return newArr
+}
+
+// Simplified
+
+function maps(x){
+  return x.map(n => n * 2);
+}
+
+// Simplified further
+
+maps = x => x.map(e => e * 2);
