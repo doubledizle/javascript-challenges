fde8c59b96a9f09ee5e3cc73b1d4c480bb37b80f
doubledizle <donnyduncan@gmail.com>
2022-03-22 00:01:55 -0500

    Update sum-of-positive-numbers-in-array.js

--- sum-of-positive-numbers-in-array.js
+++ sum-of-positive-numbers-in-array.js
@@ -1,7 +1,7 @@
 // Write a function that takes numbers from an array and adds all of the positive numbers
 
 function positiveSum(arr){
-  var total = 0;    
+  let total = 0;    
   for (i = 0; i < arr.length; i++){
     if (arr[i] > 0){
       total += arr[i];
