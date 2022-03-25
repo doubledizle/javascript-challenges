d01a69cffb91648f2495b8cc028b3b46793bce73
Donny Duncan <donnyduncan@gmail.com>
2022-03-24 19:39:52 -0500

    3/24/2022 - Refactoring prior challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

--- calculate-if-cuboid-has-equal-sides.js
+++ calculate-if-cuboid-has-equal-sides.js
@@ -1,5 +1,10 @@
 /* Write a functino that takes a cuboids volume and single side length, and returns true if the cuboid can have equal sides, false otherwise */
 
-let cubeChecker = function(volume, side){
-  return volume <= 0 || side <= 0 ? false : volume === side**3
+function cubeChecker(vol, side){
+  return volume <= 0 || side <= 0 ? false : vol === side**3
 }
+
+
+// Refactored into an arrow function
+
+const cubeChecker = (vol, side) => volume <= 0 || side <= 0 ? false : vol === side**3
