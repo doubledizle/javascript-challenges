0ebfe65670743cd21b70d8bcd14fdc0155a5e002
doubledizle <donnyduncan@gmail.com>
2022-03-27 14:53:55 -0500

    Create convert-string-to-array.js

---
+++ convert-string-to-array.js
@@ -0,0 +1,10 @@
+// Write a function that takes and string and returns and array of words from that string
+
+// Standard function syntax
+function stringToArray(string){
+  return string.split(' ');
+}
+
+// Simplified
+
+const stringToArray = string => string.split(' ') // Splits the string to create an array of words separated by a space
