d60930dd26d753e6621864014f62ad8b1da8b49f
doubledizle <donnyduncan@gmail.com>
2022-03-26 22:29:24 -0500

    Create create-array-of-numbers-up-to-n.js

---
+++ create-array-of-numbers-up-to-n.js
@@ -0,0 +1,9 @@
+// Write a function that creates an array of numbers up to and including n
+
+function numCount(n) {
+  let count = []
+  for (i = 1 ; i <= n; i++){
+    count.push(i)
+    }
+  return count
+}
