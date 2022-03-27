fadb50ee2daa7708b1b74bbb5435ba6a84171ae4
doubledizle <donnyduncan@gmail.com>
2022-03-27 17:50:56 -0500

    Create create-phone-number-from-array-of-integers.js

---
+++ create-phone-number-from-array-of-integers.js
@@ -0,0 +1,16 @@
+// Write a function that takes an array of ten integers and returns a US based phone number
+// eg: (888) 555-1212
+
+function createPhoneNumber(numbers){
+  let format = "(xxx) xxx-xxxx"
+  for(i = 0; i < numbers.length; i++) {
+    format = format.replace('x', numbers[i])
+  }
+  return format;
+}
+
+// Simplified with Reduce
+
+function createPhoneNumber(numbers){
+   return numbers.reduce((p,c) => p.replace('x',c), "(xxx) xxx-xxxx");
+}
