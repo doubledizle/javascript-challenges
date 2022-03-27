e82c6a3b9c7776831428c12b4b3c528655ae0c36
doubledizle <donnyduncan@gmail.com>
2022-03-27 01:50:49 -0500

    Create convert-binary-array-to-base-10-integer.js

---
+++ convert-binary-array-to-base-10-integer.js
@@ -0,0 +1,3 @@
+// Write a function that takes an array that is a binary number and returns the base 10 integer of that binary number
+
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2); // Join the array into a single string, then convert from base 2 to base 10 using parseInt
