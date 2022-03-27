4e59296bad2ff1ad0e09d632821f26552e650986
doubledizle <donnyduncan@gmail.com>
2022-03-26 22:16:45 -0500

    Create sum-all-numbers-of-two-arrays.js

---
+++ sum-all-numbers-of-two-arrays.js
@@ -0,0 +1,12 @@
+// Write a function that takes two arrays and returns the sum of all numbers from both arrays
+
+function arrayPlusArray(arr1, arr2) {
+  return (arr1.reduce((a, b) => a + b, 0)) + (arr2.reduce((a, b) => a + b, 0))
+}
+
+
+// Simpliefied with concat
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, c) => acc + c);
+}
