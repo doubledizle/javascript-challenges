6904cda25e1a63ab056ee6e3f76e1c3c9b86e4f5
doubledizle <donnyduncan@gmail.com>
2022-03-27 23:31:46 -0500

    Update remove-every-other-element-from-array.js

--- remove-every-other-element-from-array.js
+++ remove-every-other-element-from-array.js
@@ -1,7 +1,13 @@
 // Write a function that takes an array and removes every other elment from it starting at the second element
 
 function removeEveryOther(arr){
-  return arr.filter(function(elem, index) {
-    return index % 2 === 0;
-  });
+  let hold = []
+  for (i = 0; i < arr.length; i += 2) {
+    hold.push(arr[i])
+  }
+  return hold
 }
+
+// Simplified using Filter
+
+const removeEveryOther = arr => arr.filter((item, i) =>  i % 2 == 0);
