89d7ae6f5662520d88dcd92a3989e0903bedc875
doubledizle <donnyduncan@gmail.com>
2022-03-27 23:56:12 -0500

    Create sort-array-of-strings-return-first-modified.js

---
+++ sort-array-of-strings-return-first-modified.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array of strings and sorts them alphabetically, then returns the first element but with *** between each character
+
+function twoSort(s) {
+  const sorted = s.sort()
+  const first = sorted[0].split('').join('***')
+  return first
+}
+
+// Simplified to one line via Arrow function
+
+twoSort = s => s.sort()[0].split('').join('***')
