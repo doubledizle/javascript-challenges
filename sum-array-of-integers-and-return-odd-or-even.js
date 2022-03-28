bc4cf0db84befca940919c240bee5f3d1a01c6ab
doubledizle <donnyduncan@gmail.com>
2022-03-27 22:39:15 -0500

    Create sum-array-of-integers-and-return-odd-or-even.js

---
+++ sum-array-of-integers-and-return-odd-or-even.js
@@ -0,0 +1,21 @@
+// Write a function that takes an array of integers and returns whether the sum is an odd or even number
+
+function oddOrEven(array) {
+  let acc = 0
+  array.forEach( x => acc += +x)
+  if (acc % 2 === 0) {
+    return "even"
+  }else{
+    return "odd"
+  }
+}
+
+// Simplified with Reduce
+
+function oddOrEven(arr) {
+  return arr.reduce((acc, x)=> acc + +x, 0) % 2 ? 'odd' : 'even';
+}
+
+// Further simplified to a single line
+
+const oddOrEven = a => a.reduce((acc, x) => acc + +x, 0) %2 === 0 ? "even" : "odd";
