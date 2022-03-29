cf70562ed448f2ef0e06be5cb2be0ec02428f99c
doubledizle <donnyduncan@gmail.com>
2022-03-28 21:32:12 -0500

    Create fill-array-with-numbers-up-to-N.js

---
+++ fill-array-with-numbers-up-to-N.js
@@ -0,0 +1,9 @@
+// Write a function that takes a number and returns an array of sequential numbers starting at 0 and ending at N
+
+function arr(N) {
+  let newArr = []
+  for (i = 0; i < N; i++) {
+    arr.push(i)
+  }
+  return newArr
+}
