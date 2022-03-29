c28fb57d4c27a5a61e8fc35d0880a1a26692e107
doubledizle <donnyduncan@gmail.com>
2022-03-28 21:24:35 -0500

    Create filter-array-with-another-array.js

---
+++ filter-array-with-another-array.js
@@ -0,0 +1,11 @@
+// Write a function that takes and array of strings and returns an array that doesn't contain words from another array
+
+function wordFilter (words) {
+  let remove = ["Can't", "Give up", "Failure", "Impossible", "Never"];
+  return words.filter(item => !remove.includes(item))
+};
+
+// Simplified using an Arrow function
+
+let remove = ["Can't", "Give up", "Failure", "Impossible", "Never"];
+words => words.filter(item => !remove.includes(item))
