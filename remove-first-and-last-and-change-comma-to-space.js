1daea15e86fc836be7e4f155f421eedb937c9598
doubledizle <donnyduncan@gmail.com>
2022-03-28 23:50:56 -0500

    Create remove-first-and-last-and-change-comma-to-space.js

---
+++ remove-first-and-last-and-change-comma-to-space.js
@@ -0,0 +1,19 @@
+// Write a function that takes a string of comma separated numbers/words/phrases, removes the first and last values
+// and returns the rmainder but with spaces instead of commas
+
+function array(arr){
+  let newArr = arr.split(',')
+  newArr.shift()
+  newArr.pop()
+  if (newArr.length === 0) {
+    return null
+  }else{
+    return newArr.join(' ')
+  }
+}
+
+// Refactored
+
+function array(arr){
+  return arr.split(",").slice(1,-1).join(" ") || null;
+}
