d09485bba760123a73801dd1a59fadcdde269607
doubledizle <donnyduncan@gmail.com>
2022-03-29 22:30:41 -0500

    Create check-array-for-value-no-loops.js

---
+++ check-array-for-value-no-loops.js
@@ -0,0 +1,3 @@
+// Write a function that takes an array and searches each element for a value and returns true or false, but you can't use a loop
+
+const check = (array, value) => array.includes(value) ? true : false
