662e41851d0abf5606ef15add5c61a38be5596cc
doubledizle <donnyduncan@gmail.com>
2022-03-29 22:56:11 -0500

    Create check-highest-lowest-values-array.js

---
+++ check-highest-lowest-values-array.js
@@ -0,0 +1,10 @@
+// Write a function that returns the lowest and the highest numbers in an array
+
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)]
+}
+
+
+// Refactored into an anonymous function
+
+const minMax = arr => [Math.min(...arr), Math.max(...arr)]
