3cce098b09fd14da36233211bac313f893e1298e
Donny Duncan <donnyduncan@gmail.com>
2022-04-02 23:15:24 -0500

    4/2/2022 - Refactoring javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

--- get-closest-square-number.js
+++ get-closest-square-number.js
@@ -1,5 +1,10 @@
 // Write a function that takes a number and returns the closest square number to it
 
 function nearestSq(n){
-  return Math.pow(Math.round(Math.sqrt(n)), 2);
+  return Math.pow(Math.round(Math.sqrt(n)), 2)
 }
+
+
+// Refactored into an arrow function
+
+const nearestSq = n => Math.pow(Math.round(Math.sqrt(n)), 2)
