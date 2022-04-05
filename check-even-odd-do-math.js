5d0f513dd69212fbafd2e68043123849114111b0
Donny Duncan <donnyduncan@gmail.com>
2022-04-04 23:46:01 -0500

    4/4/2022 - More Javascript Challenge solutions
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

---
+++ check-even-odd-do-math.js
@@ -0,0 +1,3 @@
+// Write a function that checks if inputs are even or add and does a different calculation based on that
+
+const multiplyBy = number => number % 2 === 0 ? number * 3 : number * 7
---
+++ do-math-on-input-return-boolean.js
@@ -0,0 +1,3 @@
+// Write a function that takes multiple values and returns true or false depending on caluclation of those values
+
+const valueCheck = (a, b, c) => a * b >= c ? true : false
