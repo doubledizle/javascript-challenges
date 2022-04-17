e7e4b382b53709991c7d9abc7c951e8162960769
Donny Duncan <donnyduncan@gmail.com>
2022-04-16 20:09:31 -0500

    4/16/2022 - Javascript challenge
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

---
+++ check-value-return-string.js
@@ -0,0 +1,5 @@
+// Write a function that takes an argument and returns a specific string based on the value of the argument.
+
+const ageCheck = age =>
+  age < 10 ? 'drink juice' :
+  age < 21 ? 'drink soda' : 'drink beer'
