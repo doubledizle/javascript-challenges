9cd87c1fe275d1e46db6a842695aafa1ceb9a1ac
Donny Duncan <donnyduncan@gmail.com>
2022-04-20 18:20:23 -0500

    4/20/2022 - Javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

---
+++ convert-number-to-binary.js
@@ -0,0 +1,12 @@
+// Write a function that takes an integer or number string and returns the binary value of the supplied number
+
+// This function does not work properly on negative numbers
+
+function toBinary(num){
+  return Number(num.toString(2))
+}
+
+
+// Refactored into an anonymous function
+
+const toBinary = num => Number(num.toString(2))
