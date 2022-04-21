d01e19f21442ac1eed2138d0c5b13cf55f4e0f8f
Donny Duncan <donnyduncan@gmail.com>
2022-04-20 23:23:30 -0500

    4/20/2022 - Javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

---
+++ return-surface-area-and-volume-of-cube.js
@@ -0,0 +1,19 @@
+// Write a function that takes the width, heigh, and depth of a cube and returns the surface area and the volume within an array
+
+function getSize(w, h, d) {
+  let newArr = []
+  newArr.push((w * h + w * d + h * d) * 2, (w * h * d))
+  return newArr
+}
+
+
+// Refactored to use less code
+
+function getSize(w, h, d) {
+  return [(w * h + w * d + h * d) * 2, (w * h * d)]
+}
+
+
+// Refactored into an anonymous function
+
+const getSize = (w, h, d) => [(w * h + w * d + h * d) * 2, (w * h * d)]
---
+++ sum-all-positive-numbers-in-array.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array and returns the sum of only the positive numbers from that array
+
+function positivSum(arr) {
+  return arr.reduce((acc,c) => acc + (c >= 0 ? c : 0), 0) // check for positive number, add 0 if negative
+}
+
+
+// Refactored into an anonymous function
+
+const positiveSum = arr => arr.reduce((acc,c) => acc + (c >= 0 ? c : 0), 0)
