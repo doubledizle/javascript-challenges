98e934a8eca2d46ea91385cf2b24860b1f74f2f0
Donny Duncan <donnyduncan@gmail.com>
2022-05-21 21:12:14 -0500

    5/21/2022 - Javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

---
+++ return-next-perfect-square.js
@@ -0,0 +1,9 @@
+/* Write a function that takes a number and returns the next perfect square if the given number is a perfect square; return -1 otherwise */
+
+// Check if the number is a perfect square
+// If true, get square root, add 1, and square that for next perfect square, else return -1
+
+
+function findNextSquare(sq) {
+  return Number.isInteger(Math.sqrt(sq)) ? (Math.sqrt(sq) + 1)**2 : -1
+}
