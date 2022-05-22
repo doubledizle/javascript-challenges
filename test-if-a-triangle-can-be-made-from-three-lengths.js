d5efb5aae81a4b0021cff31cdd6385fb341af458
Donny Duncan <donnyduncan@gmail.com>
2022-05-21 20:12:47 -0500

    5/21/2022 - Refactoring Javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

--- test-if-a-triangle-can-be-made-from-three-lengths.js
+++ test-if-a-triangle-can-be-made-from-three-lengths.js
@@ -3,5 +3,12 @@
 
 function isTriangle(a,b,c) {
   let arr = [a,b,c].sort((a, z) => a - z)
-  return a <= 0 || b <= 0 || c <= 0 ? false : arrrr[0] + arr[1] > arr[2]
+  return a <= 0 || b <= 0 || c <= 0 ? false : arr[0] + arr[1] > arr[2]
+}
+
+
+// Refactored to simply compare sides without sorting
+
+function isTriangle(a,b,c) {
+   return a + b > c && a + c > b && c + b > a
 }
