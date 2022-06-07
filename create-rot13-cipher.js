0febef77f6dbb03c99d1a4f3f21fa6f18303f78b
Donny Duncan <donnyduncan@gmail.com>
2022-06-06 23:25:32 -0500

    6/5/2022 - Javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

---
+++ create-rot13-cipher.js
@@ -0,0 +1,20 @@
+// Write a function that takes a string as a message and returns the message encoded with a Rot13 cipher
+
+function rot13(message){
+  const alphabetLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  const alphabetUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  
+  function encrypt(char) {
+    if (alphabetLower.includes(char)) {
+      return alphabetLower[(alphabetLower.indexOf(char) + 13) % 26]
+    }
+    else if (alphabetUpper.includes(char)) {
+      return alphabetUpper[(alphabetUpper.indexOf(char) + 13) % 26]
+    }
+    else {
+      return char
+    }
+  }
+  
+  return message.split('').map(char => encrypt(char)).join('')
+}
