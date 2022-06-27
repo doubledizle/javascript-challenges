0807e11c5f9a7001f83e12be9efba00e89e40c3d
Donny Duncan <donnyduncan@gmail.com>
2022-06-26 20:05:04 -0500

    6/26/2022 - Javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

--- check-strings-for-anagrams.js
+++ check-strings-for-anagrams.js
@@ -3,3 +3,7 @@
 function isAnagram(str1,str2) {
   return str1.toLowerCase().split('').sort().join('') == str2.toLowerCase().split('').sort().join('')
 }
+
+// Refactored into an arrow function
+
+const isAnagram = (str1,str2) => str1.toLowerCase().split('').sort().join('') == str2.toLowerCase().split('').sort().join('')
