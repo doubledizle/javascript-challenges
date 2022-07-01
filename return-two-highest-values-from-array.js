e65aa75220cec3e25d77634366ac4eba5e8ca711
Donny Duncan <donnyduncan@gmail.com>
2022-06-30 20:40:24 -0500

    6/30/2022 - Javascript challengs
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

--- return-two-highest-values-from-array.js
+++ return-two-highest-values-from-array.js
@@ -1,3 +1,10 @@
 // Write a function that takes an array and returns the two highest number values from it
 
+function twoHighest(arr) {
+  return [...new Set(arr)].sort((a,z) => z - a).slice(0,2)
+}
+
+
+// Refactored into an arrow function
+
 const twoHighest = arr => [...new Set(arr)].sort((a,z) => z - a).slice(0,2)
