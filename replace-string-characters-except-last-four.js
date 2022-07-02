2a67303ff0c1ab58daed96cd0f2efbc8b6eb25f4
Donny Duncan <donnyduncan@gmail.com>
2022-07-01 23:28:55 -0500

    7/1/2022 - Javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

---
+++ replace-string-characters-except-last-four.js
@@ -0,0 +1,7 @@
+// Write a function that takes a string of integers and replaces all but the last 4 characters with #
+
+
+// Using the Map method
+function maskify(cc) {
+  return cc.length > 4 ? cc.split('').map((e, i) => i < cc.length - 4 ? e = '#' : e = e).join('') : cc
+}
