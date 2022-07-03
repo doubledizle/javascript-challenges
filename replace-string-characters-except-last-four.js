6cc3a43f571c02be30491760f1ef459f726c9204
Donny Duncan <donnyduncan@gmail.com>
2022-07-02 23:30:54 -0500

    7/2/2022 - Refactoring Javascript challenges
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

--- replace-string-characters-except-last-four.js
+++ replace-string-characters-except-last-four.js
@@ -1,7 +1,12 @@
 // Write a function that takes a string of integers and replaces all but the last 4 characters with #
 
-
 // Using the Map method
 function maskify(cc) {
   return cc.length > 4 ? cc.split('').map((e, i) => i < cc.length - 4 ? e = '#' : e = e).join('') : cc
+}
+
+
+// Refactored using Slice and Padstart
+function maskify(cc) {
+  return cc.slice(-4).padStart(cc.length,'#')
 }
